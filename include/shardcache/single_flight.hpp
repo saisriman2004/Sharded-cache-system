7184368b2d246cfb60320e703bb11f2737f83356
saisriman2004 <saisriman034@gmail.com>
2026-08-12 19:16:07 -0700

    define SingleFlight class header in include/shardcache/single_flight.hpp

---
+++ include/shardcache/single_flight.hpp
@@ -0,0 +1,37 @@
+#ifndef SHARDCACHE_SINGLE_FLIGHT_HPP
+#define SHARDCACHE_SINGLE_FLIGHT_HPP
+
+#include <string>
+#include <functional>
+#include <unordered_map>
+#include <mutex>
+#include <condition_variable>
+#include <memory>
+
+namespace shardcache {
+
+class SingleFlight {
+public:
+    SingleFlight() = default;
+    ~SingleFlight() = default;
+
+    using FetchFn = std::function<std::string()>;
+
+    std::string do_call(const std::string& key, FetchFn fn);
+
+private:
+    struct Call {
+        std::mutex mutex;
+        std::condition_variable cv;
+        bool done{false};
+        std::string val;
+        std::string err;
+    };
+
+    std::mutex map_mutex_;
+    std::unordered_map<std::string, std::shared_ptr<Call>> calls_;
+};
+
+} // namespace shardcache
+
+#endif // SHARDCACHE_SINGLE_FLIGHT_HPP
